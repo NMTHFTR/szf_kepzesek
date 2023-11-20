bca9ede476a15976065af03a913d82bcfd69f670
NMTHFTR <benedekosze@gmail.com>
2023-11-20 13:54:52 +0100

    wuuuu

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 require('dotenv').config() // A .env fájlt olvassa
 
+
 const app = express()
 app.listen(process.env.PORT,()=> console.log(`Server running on port ${process.env.PORT}`));
+
+console.log("middlawareben vagyok");
