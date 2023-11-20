5c98953326be64d182783c1412e3daadd61af374
NMTHFTR <benedekosze@gmail.com>
2023-11-20 14:06:29 +0100

    a Képzésekhez tartózo útvonalak a rooterben

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const trainings=require('./routes/trainings')
 
 const app = express()
 
+
 app.use('/api/trainings',trainings)
 
 app.listen(process.env.PORT,()=> console.log(`Server running on port ${process.env.PORT}`));
