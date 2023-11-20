d5adb5e9633964385eb4faf76409f75c2a887413
NMTHFTR <benedekosze@gmail.com>
2023-11-20 14:05:56 +0100

    aa

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 require('dotenv').config() // A .env fájlt olvassa
-
+const trainings=require('./routes/trainings')
 
 const app = express()
+
+app.use('/api/trainings',trainings)
+
 app.listen(process.env.PORT,()=> console.log(`Server running on port ${process.env.PORT}`));
 
 
